91f47e09e563efe136f65e4749c760607e5750b1
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-30 12:36:31 +0100

    move if statement to employess for loop

--- 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
+++ 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
@@ -29,17 +29,17 @@ public class BirthdayService {
             Employee employee = new Employee(employeeData[1], employeeData[0],
                     employeeData[2], employeeData[3]);
             employees.add(employee);
-            if (employee.isBirthday(ourDate)) {
-                String recipient = employee.getEmail();
-                String body = "Happy Birthday, dear %NAME%!".replace("%NAME%",
-                        employee.getFirstName());
-                String subject = "Happy Birthday!";
-                sendMessage(smtpHost, smtpPort, "sender@here.com", subject,
-                        body, recipient);
-            }
         }
 
         for (Employee employee : employees) {
+            if (employee.isBirthday(ourDate)) {
+                String recipient = employee.getEmail();
+                String body = "Happy Birthday, dear %NAME%!".replace("%NAME%",
+                    employee.getFirstName());
+                String subject = "Happy Birthday!";
+                sendMessage(smtpHost, smtpPort, "sender@here.com", subject,
+                    body, recipient);
+            }
         }
     }
 
