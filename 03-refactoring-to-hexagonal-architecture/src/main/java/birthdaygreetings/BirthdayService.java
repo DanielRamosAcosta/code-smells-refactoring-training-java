4bdd84519f43920f0fb53190cfd71be6bae9487b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-30 12:33:39 +0100

    add employees empty list

--- 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
+++ 03-refactoring-to-hexagonal-architecture/src/main/java/birthdaygreetings/BirthdayService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -21,6 +23,7 @@ public class BirthdayService {
         BufferedReader in = new BufferedReader(new FileReader(fileName));
         String str = "";
         str = in.readLine(); // skip header
+        List<Employee> employees = new ArrayList<>();
         while ((str = in.readLine()) != null) {
             String[] employeeData = str.split(", ");
             Employee employee = new Employee(employeeData[1], employeeData[0],
