def537d987d8fc4edaf043d73c4ca6d5fb392dfa
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 00:44:09 +0100

    remove while

--- 02-refactoring-video-store/src/main/java/video_store/Customer.java
+++ 02-refactoring-video-store/src/main/java/video_store/Customer.java
@@ -1,15 +1,25 @@
 package video_store;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 import java.util.Enumeration;
 
 public class Customer {
+    private final String name;
+
+    private final Vector rentals2 = new Vector();
+
+    private final List<Rental> rentals = new ArrayList<>();
+
+
     public Customer(String name) {
         this.name = name;
     }
 
     public void addRental(Rental rental) {
-        rentals.addElement(rental);
+        rentals2.addElement(rental);
+        rentals.add(rental);
     }
 
     public String getName() {
@@ -19,16 +29,13 @@ public class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration rentals = this.rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
-        while (rentals.hasMoreElements()) {
-            Rental rental = (Rental) rentals.nextElement();
+        for (Rental rental : this.rentals) {
             double thisAmount = rental.calculateAmount();
             frequentRenterPoints = frequentRenterPoints + rental.computeFrequentRenterPoints();
             result += "\t" + rental.getMovie().getTitle() + "\t" + thisAmount + "\n";
             totalAmount += thisAmount;
-
         }
 
         result += "You owed " + totalAmount + "\n";
@@ -36,8 +43,4 @@ public class Customer {
 
         return result;
     }
-
-
-    private String name;
-    private Vector rentals = new Vector();
 }
