1f86bc05029d46958aa882cb6d31f58f36c355ae
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 00:41:40 +0100

    refactor

--- 02-refactoring-video-store/src/main/java/video_store/Customer.java
+++ 02-refactoring-video-store/src/main/java/video_store/Customer.java
@@ -25,29 +25,8 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental rental = (Rental) rentals.nextElement();
-
-            // determines the amount for rental line
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (rental.getDaysRented() > 2)
-                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            frequentRenterPoints++;
-
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE
-                && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            thisAmount = rental.calculateAmount();
+            frequentRenterPoints = frequentRenterPoints + rental.computeFrequentRenterPoints();
 
             result += "\t" + rental.getMovie().getTitle() + "\t"
                 + thisAmount + "\n";
