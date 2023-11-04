0584b792d76b1243b501cf05a1b43dcb61d42995
Daniel Ramos <danielramosacosta1@gmail.com>
2023-11-04 01:18:16 +0100

    remove pseudoenum

--- 02-refactoring-video-store/src/main/java/video_store/Movie.java
+++ 02-refactoring-video-store/src/main/java/video_store/Movie.java
@@ -1,25 +1,13 @@
 package video_store;
 
 public class Movie {
-    public static final int CHILDRENS = 2;
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-
     private final String title;
-
-    public static Movie create(String title, int priceCode) {
-        return switch (priceCode) {
-            case CHILDRENS -> childrens(title);
-            case NEW_RELEASE -> getNewReleaseMovie(title);
-            default -> regular(title);
-        };
-    }
 
     public static Movie regular(String title) {
         return new Movie(title);
     }
 
-    public static NewReleaseMovie getNewReleaseMovie(String title) {
+    public static NewReleaseMovie newRelease(String title) {
         return new NewReleaseMovie(title);
     }
 
